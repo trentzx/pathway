b24d3b7acc95ecb33dce756c1df06f308ebd4538
amazona1exa <104154128+amazona1exa@users.noreply.github.com>
2023-10-05 10:19:58 -0400

    Day 6..

--- Assets/Scripts/GhostBehaviour.cs
+++ Assets/Scripts/GhostBehaviour.cs
@@ -1,27 +1,34 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Ghost))]
-public class GhostBehaviour : MonoBehaviour
+public abstract class GhostBehavior : MonoBehaviour
 {
     public Ghost ghost { get; private set; }
-}
-        private void Awake()
-{
-    this.ghost = GetComponent<Ghost>();
-    this.enabled = false;
-}
+    public float duration;
 
-public void Enable()
-{
+    private void Awake()
+    {
+        ghost = GetComponent<Ghost>();
+    }
 
-}
+    public void Enable()
+    {
+        Enable(duration);
+    }
 
-public void Enable(float duration)
-{
+    public virtual void Enable(float duration)
+    {
+        enabled = true;
 
-}
+        CancelInvoke();
+        Invoke(nameof(Disable), duration);
+    }
 
-public void Disable()
-{
+    public virtual void Disable()
+    {
+        enabled = false;
+
+        CancelInvoke();
+    }
 
 }
